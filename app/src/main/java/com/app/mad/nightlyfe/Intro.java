04cd3359f55c1fdc66d749e3e56ab5cfa2314426
Dennis Nielsen <nevadaa51@yahoo.se>
2016-05-13 18:58:36 +0200

    first database commit

--- app/src/main/java/com/app/mad/nightlyfe/Intro.java
+++ app/src/main/java/com/app/mad/nightlyfe/Intro.java
@@ -1,7 +1,9 @@
 package com.app.mad.nightlyfe;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class Intro extends AppCompatActivity {
 
@@ -9,13 +11,18 @@ public class Intro extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        VenueDB db = new VenueDB(this);
+
+        db.insertVenue("Sticky Fingers", "Rock", "17.00-03.00", 60, "Grönsakstorget", "medium crowded");
+
+        Cursor rs = db.getData(1);
+        rs.moveToFirst();
+        String venue = rs.getString(rs.getColumnIndex(VenueDB.VENUES_COLUMN_VENUE_NAME));
+
+       TextView venueName = (TextView) findViewById(R.id.venueName);
+        venueName.setText(venue);
+
     }
 
-    //  Just a test comment
-
-    /*
-    Another test comment, hope this stupid ass shit finally fucking works
-     */
-
-    //tesst 123
 }
---
+++ app/src/main/java/com/app/mad/nightlyfe/VenueDB.java
@@ -0,0 +1,62 @@
+package com.app.mad.nightlyfe;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Dennis on 2016-05-13.
+ */
+public class VenueDB extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "VenueMusic.db";
+    //public static final String VENUES_TABLE_NAME = "contacts";
+    public static final String VENUES_COLUMN_ID = "id";
+    public static final String VENUES_COLUMN_VENUE_NAME = "venue_name";
+    public static final String VENUES_COLUMN_GENRE = "genre";
+    public static final String VENUES_COLUMN_OPENING_HOURS = "opening_hours";
+    public static final String VENUES_COLUMN_ENTRANCE_FEE= "entrance_fee";
+    public static final String VENUES_COLUMN_ADDRESS = "address";
+    public static final String VENUES_COLUMN_CROWDED = "crowded";
+
+    public VenueDB(Context context)
+    {
+        super(context, DATABASE_NAME , null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(
+                "create table venues " +
+                        "(id integer primary key, venue_name text,genre text,opening_hours text, entrance_fee integer, address text, crowded text)"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public boolean insertVenue  (String venue_name, String genre, String opening_hours, int entrance_fee,String address, String crowded)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("venue_name", venue_name);
+        contentValues.put("genre", genre);
+        contentValues.put("opening_hours", opening_hours);
+        contentValues.put("entrance_fee", entrance_fee);
+        contentValues.put("address", address);
+        contentValues.put("crowded", crowded);
+        db.insert("venues", null, contentValues);
+        return true;
+    }
+
+    public Cursor getData(int id){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res =  db.rawQuery( "select * from venues where id="+id+"", null );
+        return res;
+    }
+
+}
